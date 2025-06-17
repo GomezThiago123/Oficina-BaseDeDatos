3ef67799707df5b79d0745613726541f819b352e
Thiago Gomez <thiagogomez12300@gmail.com>
2025-06-17 14:26:14 -0300

    Hice el InsertOficina a medias, Miguel lo va completar.

---
+++ Oficina/InsertOficina.sql
@@ -0,0 +1,8 @@
+INSERT INTO Departamento (Nombre, Piso, empleados_id, departamento_id)
+VALUES
+('Recepción', 1, 10, 1)
+('Baños', 1, 5, 2)
+('Baños', 2, 6 ,2)
+('Departamento de Marketing Digital',2 , 7, 3)
+('departamento de atención al cliente', 3, 5, 4)
+()
